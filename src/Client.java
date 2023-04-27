1aa05ac5d798a482545d27e9ed365badab9fe2d3
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-27 10:56:30 -0300

    change getShape to static

--- src/Client.java
+++ src/Client.java
@@ -1,20 +1,18 @@
 public class Client {
     public static void main(String[] args) {
-        ShapeFactory shapeFactory = new ShapeFactory();
-
         //get an object of Circle and call its draw method.
-        Shape shape1 = shapeFactory.getShape("Circle");
+        Shape shape1 = ShapeFactory.getShape("Circle");
         //call draw method of Circle
         shape1.draw();
 
         //get an object of Rectangle and call its draw method.
-        Shape shape2 = shapeFactory.getShape("Rectangle");
+        Shape shape2 = ShapeFactory.getShape("Rectangle");
         //call draw method of Rectangle
         shape2.draw();
 
 
         //get an object of Square and call its draw method.
-        Shape shape3 = shapeFactory.getShape("Square");
+        Shape shape3 = ShapeFactory.getShape("Square");
         //call draw method of Square
         shape3.draw();
     }
--- src/ShapeFactory.java
+++ src/ShapeFactory.java
@@ -1,6 +1,6 @@
 public class ShapeFactory {
     //use getShape method to get object of type shape
-    public Shape getShape(String shapeType) {
+    public static Shape getShape(String shapeType) {
         if(shapeType == null) {
             return null;
         } else if (shapeType.equalsIgnoreCase("CIRCLE")) {
